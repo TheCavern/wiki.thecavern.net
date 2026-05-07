25dac1d4056b121db7f19a05830f9347414d82a3
Nadie <Hackerjef@users.noreply.github.com>
2026-05-07 14:35:14 -0400

    Upgrades Docusaurus & add search/plugins (#18)
    * Add Dev deployment push to main :D
    * updated README.md
    * Updating Docusaurus & added mention for wiki-next
    * Added search & some seo optimization

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,10 +10,11 @@ const config = {
   organizationName: 'The Cavern',
   projectName: 'Cavern Wiki',
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
   deploymentBranch: 'gh-pages',
-
-
+  future: {
+    v4: true,
+    faster: true,
+  },
   presets: [
     [
       '@docusaurus/preset-classic',
@@ -24,10 +25,48 @@ const config = {
         },
         theme: {
           customCss: './src/css/custom.css',
-        }
+        },
+        sitemap: {
+          lastmod: 'date',
+          changefreq: 'weekly',
+          priority: 0.5,
+          ignorePatterns: ['/tags/**'],
+          filename: 'sitemap.xml',
+          createSitemapItems: async (params) => {
+            const {defaultCreateSitemapItems, ...rest} = params;
+            const items = await defaultCreateSitemapItems(rest);
+            return items.filter((item) => !item.url.includes('/page/'));
+          },
+        },
       },
     ],
   ],
+  plugins: [
+    [
+      '@easyops-cn/docusaurus-search-local',
+      {
+        hashed: true,
+        indexDocs: true,
+        indexPages: true,
+        language: 'en',
+      },
+    ],
+    [
+      '@docusaurus/plugin-ideal-image',
+      {
+        quality: 70,
+        max: 1030, // max resized image's size.
+        min: 640, // min resized image's size. if original is lower, use that size.
+        steps: 2, // the max number of images generated between min and max (inclusive)
+        disableInDev: false,
+      },
+    ],
+  ],
+  markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: 'warn',
+    }
+  },
 
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
@@ -46,6 +85,13 @@ const config = {
           },
         ],
       },
+      announcementBar: process.env.DEPLOY_ENV === 'dev' ? {
+        id: 'dev-build-warning',
+        content: 'This is a pre-release version of our wiki, can contain errors',
+        backgroundColor: '#fa7d10',
+        textColor: '#000',
+        isCloseable: false,
+      } : undefined,
       prism: {
         theme: prismThemes.github,
         darkTheme: prismThemes.dracula,
